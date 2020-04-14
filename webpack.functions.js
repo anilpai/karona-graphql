3ea7844aec16ab2841bee72899bb554f3e2734b0
Anil Pai <anilbpai@gmail.com>
2020-04-14 01:45:32 -0500

    Fixed netlify lambda build issue
    Error: Can't resolve 'bufferutil' & 'utf-8-validate' in '/opt/build/repo/node_modules/ws/lib'

---
+++ webpack.functions.js
@@ -0,0 +1,14 @@
+const nodeExternals = require(`webpack-node-externals`);
+
+module.exports = {
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.graphql$/,
+        use: [{ loader: `graphql-import-loader` }]
+      }
+    ]
+  }
+};
